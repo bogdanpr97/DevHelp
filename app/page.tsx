3dc769f6fd4793a6981d59b6367da3e005254701
bogdanpreda97 <bobipreda@gmail.com>
2025-07-12 20:47:46 +0300

    add assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="h1-bold">Hello Next</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Hello inter</h1>
+      <h1 className="h1-bold font-space-grotesk">Hello space</h1>
+    </>
+  );
 }
